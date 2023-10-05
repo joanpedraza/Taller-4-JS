9d0921806637ff92d314dad1fdea66e82d278e57
Wissbegierde <juandavidmenag@gmail.com>
2023-10-05 00:32:21 -0500

    Chc_Usuarios

---
+++ formulario.js
@@ -0,0 +1,105 @@
+const nombre=document.getElementById("name");
+const apellidos=document.getElementById("apellidos");
+const direccion=document.getElementById("direccion");
+const telefono=document.getElementById("telefono");
+const email=document.getElementById("email");
+const usuario=document.getElementById("nusuario");
+const password=document.getElementById("password");
+const Vpassword=document.getElementById("Vpassword");
+const form=document.getElementById("form");
+const parrafo=document.getElementById("warnings");
+
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    let warnings=" ";
+    let RegexEmail=/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let entrar=false;
+    let tieneMayusculas = /[A-Z]/.test(password.value);
+    let tieneNumeros = /[0-9]/.test(password.value);
+    let tieneLetras = /[a-zA-Z]/.test(password.value);
+    let tieneCaracteresEspeciales = /[#%&/]/.test(password.value);
+    let SoloNumeros = /^\d+$/;
+
+    parrafo.innerHTML=" ";
+
+    if (nombre.value.length <= 6) {
+       warnings+='El nombre no es válido.<br>';
+       entrar=true;
+    }
+    if (apellidos.value.length <= 6) {
+        warnings+='El apellido no es válido.<br>';
+        entrar=true;
+     }
+
+     if (
+        !(
+          direccion.value.startsWith('cll') ||
+          direccion.value.startsWith('cra') ||
+          direccion.value.startsWith('av')  ||
+          direccion.value.startsWith('anv') ||
+          direccion.value.startsWith('trans')
+        )
+        ){
+        warnings += 'La dirección debe comenzar con al menos "cll, cra, av, anv, trans".<br>';
+        entrar = true;
+      }
+     
+
+    if(!SoloNumeros.test(telefono.value) && (telefono.value<10)){
+        warnings+='El numero no es válido,tiene que contener solo numeros.<br>';
+        entrar=true;
+    }
+
+    switch (true) {
+        case !SoloNumeros.test(telefono.value):
+            warnings+='El telefono tiene que contener solo numeros.<br>';
+          break;
+        case telefono.value.length < 10:
+            warnings+='El telefono tiene que contener 10 digitos.<br>';
+          break;
+      }
+
+    if(!RegexEmail.test(email.value)){
+    warnings+='El email no es válido.<br>';
+    entrar=true;
+    }
+    if(usuario.value.length<10){
+        warnings+='el usuario es muy corto.<br>';
+        entrar=true;
+    }
+    if(/[^a-zA-Z0-9]+/.test(usuario.value)){
+        warnings+='el usuario no debe contener letras especiales. <br>';
+        entrar=true;
+    }
+    if (!(tieneMayusculas && tieneNumeros && tieneLetras && tieneCaracteresEspeciales)) {
+        switch (true) {
+            case !tieneMayusculas:
+                warnings += 'La contraseña  Debe contener al menos una letra mayúscula.<br>';
+            case !tieneNumeros:
+                warnings += 'La contraseña  Debe contener al menos un número.<br>';
+                
+            case !tieneLetras:
+                warnings += 'La contraseña  Debe contener al menos una letra.<br>';
+               
+            case !tieneCaracteresEspeciales:
+                warnings += 'La contraseña  Debe contener al menos un carácter especial (#, %, /, &).<br>';
+             
+            default:
+                warnings += 'La contraseña no es válida.<br>';
+        }
+    
+        entrar = true;
+    }
+    if(password.value.length < 15){
+        warnings += 'La contraseña tiene que tener al menos 15 letras.<br>';
+        entrar=true;
+    }
+    if(password.value!==Vpassword.value){
+        warnings+='las contraseñas tienen que ser iguales. <br>'
+        entrar=true;
+    }
+
+    if(entrar==true){
+        parrafo.innerHTML=warnings;
+    }
+});
